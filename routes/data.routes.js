4f1f0afde8a398565c7b2e0ba6ab6ae45bf40b63
Prakashkumarjena700 <prakashkumarjena367@gmail.com>
2023-03-03 01:28:49 +0530

    Added filter, sort, pagination

--- routes/data.routes.js
+++ routes/data.routes.js
@@ -4,10 +4,60 @@ const { dataModel } = require("../model/data.model")
 const dataRoute = express.Router()
 
 dataRoute.get("/", async (req, res) => {
-    const user_id_making_req = req.body.user
+    const { q, limit, skip, sort, order, type, gender, rating, color, size } = req.query
+
+    const query = { dis: { $regex: q, $options: "i" } }
+
+    let x;
+    if (q == undefined && gender == undefined && rating === undefined && color == undefined && type == undefined && size == undefined) {
+        x = {}
+    } else if (q !== undefined && gender == undefined && color == undefined && rating === undefined && type == undefined && size == undefined) {
+        x = query
+    } else if (q == undefined && gender !== undefined && color == undefined && rating === undefined && type == undefined && size == undefined) {
+        x = { gender }
+    } else if (q == undefined && gender !== undefined && color !== undefined && rating !== undefined && type != undefined && size == undefined) {
+        x = { color, gender, rating, type }
+    } else if (q == undefined && gender == undefined && rating === undefined && color !== undefined && type == undefined && size == undefined) {
+        x = { color }
+    } else if (q == undefined && gender == undefined && rating !== undefined && color !== undefined && type == undefined && size == undefined) {
+        x = { rating, color }
+    } else if (q == undefined && gender !== undefined && rating === undefined && color !== undefined && type == undefined && size == undefined) {
+        x = { color, gender }
+    } else if (q == undefined && gender !== undefined && rating !== undefined && color == undefined && type == undefined && size == undefined) {
+        x = { gender, rating }
+    } else if (q == undefined && gender == undefined && rating == undefined && color == undefined && type !== undefined && size == undefined) {
+        x = { type }
+    } else if (q == undefined && gender == undefined && rating == undefined && color !== undefined && type !== undefined && size == undefined) {
+        x = { type, color }
+    } else if (q == undefined && gender == undefined && rating !== undefined && color == undefined && type !== undefined && size == undefined) {
+        x = { type, rating }
+    } else if (q == undefined && gender !== undefined && rating == undefined && color == undefined && type !== undefined && size == undefined) {
+        x = { type, gender }
+    } else if (q == undefined && gender === undefined && rating == undefined && color == undefined && type === undefined && size !== undefined) {
+        x = { size: { $in: [size] } }
+    } else if (q == undefined && gender === undefined && rating == undefined && color == undefined && type !== undefined && size !== undefined) {
+        x = { size: { $in: [size] }, type }
+    } else if (q == undefined && gender === undefined && rating == undefined && color !== undefined && type !== undefined && size !== undefined) {
+        x = { size: { $in: [size] }, type, color }
+    } else if (q == undefined && gender === undefined && rating !== undefined && color !== undefined && type !== undefined && size !== undefined) {
+        x = { size: { $in: [size] }, type, color, rating }
+    } else if (q == undefined && gender !== undefined && rating !== undefined && color !== undefined && type !== undefined && size !== undefined) {
+        x = { size: { $in: [size] }, type, color, rating, gender }
+    }
+
+    let y = {}
+    if (sort == 'rating') {
+        y = { rating: order }
+    } else if (sort == 'price') {
+        y = { price: order }
+    } else if (sort == 'name') {
+        y = { name: order }
+    }
+
     try {
-        let data = await dataModel.find({ user: user_id_making_req })
+        let data = await dataModel.find(x).sort(y).limit(limit).skip(skip)
         res.send(data)
+
     } catch (err) {
         res.send("Can't find product")
     }
